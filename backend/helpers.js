15d5ebc34d526055a0731ece7b29ce5d6537b07f
Teresa Pham <tkypham@gmail.com>
2020-12-26 13:40:27 +1100

    Setup backend for app

---
+++ backend/helpers.js
@@ -0,0 +1,6 @@
+/*
+  This is a file of data and helper functions that we can expose and use in our templating function
+*/
+
+// Dump is a handy debugging function we can use to sort of "console.log" our data
+exports.dump = (obj) => JSON.stringify(obj, null, 2);
